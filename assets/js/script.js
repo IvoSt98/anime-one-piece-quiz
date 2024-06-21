8c53eb92f091b5fd6c51611ad25e7ebc4efe629a
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-06-21 19:59:24 +0000

    In scrupt.js were added an array for all of the questions,choices and the correct answer for each question

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Questions, options and asnwers array
+const quizQuestions = [{
+    question: "Who promised that they would never lose another fight until they defeated a certain someone?",
+    choices: ["Luffy", "Sanji", "Zoro", "Usopp"],
+    answer: "Zoro"
+},
+{
+    question: "How did Luffy get the scar under his eye?",
+    choices: ["Atacked by bandits", "Fight with animals", "Himself", "Fight with another villager"],
+    answer: "Himself"
+},
+{
+    question: "Who was the first member of the SH crew to try and recruit a new member besides Luffy?",
+    choices: ["Nami", "Sanji", "Zoro", "Usopp"],
+    answer: "Nami"
+},
+{
+    question: "What made Crocodile join Luffy's 'Rescue Ace Crew'?",
+    choices: ["Luffy", "Jinbei", "Ace", "Ivancov"],
+    answer: "Ace"
+},
+{
+    question: "Who was the first marine Admiral to be shown in the series?",
+    choices: ["Akainu", "Kizaru", "Aokiji", "Sengoku"],
+    answer: "Aokiji"
+},
+{
+    question: "Who gave Shanks the scar on his eye?",
+    choices: ["Dragon", "Marshall D", "Akainu", "Himself"],
+    answer: "Marshall D"
+},
+{
+    question: "How many crewmates did Luffy say he wanted at the beginning of the series?",
+    choices: ["9", "13", "10", "15"],
+    answer: "10"
+},
+{
+    question: "Who said- Zoro should cut diamond next?",
+    choices: ["Luffy", "Mihawk", "Kuina", "Daz Bones"],
+    answer: "Kuina"
+},
+{
+    question: "Who was the first recruit of the SH crew?",
+    choices: ["Nami", "Sanji", "Zoro", "Usopp"],
+    answer: "Zoro"
+},
+{
+    question: "How many confirmed D clan members are there?",
+    choices: ["8", "14", "11", "18"],
+    answer: "14"
+},
+];
